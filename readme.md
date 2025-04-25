67ee01f222d05e2ffb8c045d5afba0e8adb2b987
kkamara <kamaracomputers@gmail.com>
2025-04-25 19:31:28 +0100

    Remove Git history and add project due to code repository being too large

---
+++ readme.md
@@ -0,0 +1,76 @@
+<img src="https://codedthemes.com/wp-content/uploads/edd/2022/05/Materially-ReactJS-Admin-Dashboard.webp" alt="Materially-ReactJS-Admin-Dashboard.webp" width=""/>
+
+<img src="https://github.com/kkamara/useful/blob/main/kelvinkamara.png?raw=true" alt="kelvinkamara.png" width=""/>
+
+# Kelvin Kamara
+
+(05-Apr-2023) Soundscape, background noise app. To focus on your task and entertainment. Electronjs, Reactjs 16.
+
+- We can do it and it's not just do it.
+
+- What are they tryna tell you?
+
+- Erm...
+
+## Download Latest
+
+* [Windows app v4.20.0](https://github.com/kkamara/kelvinkamara/releases).
+
+## Requirements
+
+* [NodeJS v14.21.3](https://nodejs.org/en/blog/release/v14.21.3) (comes with [NPM](https://www.npmjs.com/)).
+
+## Installation
+
+```bash
+  npm install
+```
+
+## Usage
+
+```bash
+  npm start
+```
+
+## Building target: cross-platform
+
+```bash
+  # --linux --win --mac --x64 --ia32
+  npm run build
+```
+
+## More
+
+The Kelvin Kamara sound app planning design.
+
+<img src="https://raw.githubusercontent.com/kkamara/useful/main/kelvinkamara-planning.png" alt="kelvinkamara-planning.png" width="265"/> <img src="https://raw.githubusercontent.com/kkamara/useful/main/kelvinkamara-planning2.png" alt="kelvinkamara-planning2.png" width="265"/>
+
+* [Building KelvinKamara](https://github.com/kkamara/useful/blob/main/kelvinkamara-building.png)
+* [Building KelvinKamara 2](https://github.com/kkamara/useful/blob/main/kelvinkamara-building2.png)
+* [Building KelvinKamara 3](https://github.com/kkamara/useful/blob/main/kelvinkamara-building3.png)
+* [Building KelvinKamara 4](https://github.com/kkamara/useful/blob/main/kelvinkamara-building4.png)
+* [Building KelvinKamara 5](https://github.com/kkamara/useful/blob/main/kelvinkamara-building5.png)
+* [Desktop 2023](https://raw.githubusercontent.com/kkamara/useful/main/desktop-2023.png)
+* [Desktop 2023 extra](https://github.com/kkamara/useful/commit/a9e620925598c945ad71501388dc615f4b381d33) (comments at the bottom of the page).
+
+## Misc
+
+The audio players that play atmospheric sound change time every 10 minutes.
+
+PTSD Healing (64kbps): https://drive.google.com/file/d/1VXYLEFUG6GB8jz-0f9cRcvCPje2EZSOB/view?usp=sharing
+
+[See Multi-Window Desktop App](https://github.com/kkamara/multi-window-desktop-app).
+
+[See ReactJS Boilerplate.](https://github.com/kkamara/react-boilerplate).
+
+[See PHP ReactJS Boilerplate.](https://github.com/kkamara/php-reactjs-boilerplate).
+
+[See Node ReactJS Boilerplate.](https://github.com/kkamara/nodejs-reactjs-boilerplate).
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[BSD](https://opensource.org/licenses/BSD-3-Clause)
---
+++ src/views/Dashboard/Default/SoundCard/AsmrFlashlightCard.js
@@ -0,0 +1,163 @@
+import React from 'react';
+import { useTheme } from '@material-ui/styles';
+import {  Card, CardContent, CardHeader, Divider, Grid, Typography, } from '@material-ui/core';
+import { gridSpacing, } from '../../../../store/constant';
+
+import ReactAudioPlayer from 'react-audio-player';
+import AsmrFlashlightSound from '../../../../assets/audio/ASMR-Bright-Light-Triggers-_Click-Sounds_-Gentle-Eye-Tests-&-Instructions-64-kbps.mp3';
+
+const soundStyles = {
+    width: '285px',
+};
+
+const AsmrFlashlightCard = (props) => {
+    const theme = useTheme();
+
+    return (
+        <Card>
+          <CardHeader
+              title={
+                  <Typography component="div" className="card-header">
+                      ASMR Flashlight Sound <i>(64kbps)</i>
+                  </Typography> 
+              }
+          />
+          <Divider />
+          <CardContent>
+              <Grid container spacing={gridSpacing}>
+                  <Grid item xs={12}>
+                      <Grid container alignItems="center" spacing={1}>
+                          <Grid item xs={12}>
+                              <label>
+                                  ASMR Bright Light Triggers *Click Sounds* 
+                                  Gentle Eye Tests & Instructions <i>(64kbps)</i>
+                              </label>
+                              <ReactAudioPlayer
+                                  // id="firstAudioPlayer"
+                                  src={AsmrFlashlightSound}
+                                  loop
+                                  controls
+                                  style={soundStyles}
+                              />
+                          </Grid>
+                      </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                      <Grid container alignItems="center" spacing={1}>
+                          <Grid item xs={12}>
+                              <label>
+                                ASMR Bright Light Triggers *Click Sounds* 
+                                Gentle Eye Tests & Instructions <i>(64kbps)</i>
+                              </label>
+                              <ReactAudioPlayer
+                                  src={AsmrFlashlightSound}
+                                  loop
+                                  controls
+                                  style={soundStyles}
+                              />
+                          </Grid>
+                      </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                      <Grid container alignItems="center" spacing={1}>
+                          <Grid item xs={12}>
+                              <label>
+                                ASMR Bright Light Triggers *Click Sounds* 
+                                Gentle Eye Tests & Instructions <i>(64kbps)</i>
+                              </label>
+                              <ReactAudioPlayer
+                                src={AsmrFlashlightSound}
+                                loop
+                                controls
+                                style={soundStyles}
+                              />
+                          </Grid>
+                      </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                      <Grid container alignItems="center" spacing={1}>
+                          <Grid item xs={12}>
+                            <label>
+                              ASMR Bright Light Triggers *Click Sounds* 
+                              Gentle Eye Tests & Instructions <i>(64kbps)</i>
+                            </label>
+                            <ReactAudioPlayer
+                              src={AsmrFlashlightSound}
+                              loop
+                              controls
+                              style={soundStyles}
+                            />
+                          </Grid>
+                      </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                      <Grid container alignItems="center" spacing={1}>
+                          <Grid item xs={12}>
+                            <label>
+                              ASMR Bright Light Triggers *Click Sounds* 
+                              Gentle Eye Tests & Instructions <i>(64kbps)</i>
+                            </label>
+                            <ReactAudioPlayer
+                              src={AsmrFlashlightSound}
+                              loop
+                              controls
+                              style={soundStyles}
+                            />
+                          </Grid>
+                      </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                      <Grid container alignItems="center" spacing={1}>
+                          <Grid item xs={12}>
+                            <label>
+                              ASMR Bright Light Triggers *Click Sounds* 
+                              Gentle Eye Tests & Instructions <i>(64kbps)</i>
+                            </label>
+                            <ReactAudioPlayer
+                              src={AsmrFlashlightSound}
+                              loop
+                              controls
+                              style={soundStyles}
+                            />
+                          </Grid>
+                      </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                      <Grid container alignItems="center" spacing={1}>
+                          <Grid item xs={12}>
+                            <label>
+                              ASMR Bright Light Triggers *Click Sounds* 
+                              Gentle Eye Tests & Instructions <i>(64kbps)</i>
+                            </label>
+                            <ReactAudioPlayer
+                              src={AsmrFlashlightSound}
+                              loop
+                              controls
+                              style={soundStyles}
+                            />
+                          </Grid>
+                      </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                      <Grid container alignItems="center" spacing={1}>
+                          <Grid item xs={12}>
+                            <label>
+                              ASMR Bright Light Triggers *Click Sounds* 
+                              Gentle Eye Tests & Instructions <i>(64kbps)</i>
+                            </label>
+                            <ReactAudioPlayer
+                              src={AsmrFlashlightSound}
+                              loop
+                              controls
+                              style={soundStyles}
+                            />
+                          </Grid>
+                      </Grid>
+                  </Grid>
+              </Grid>
+          </CardContent>
+      </Card>
+    );
+};
+
+export default AsmrFlashlightCard;
